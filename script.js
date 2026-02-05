10c8dbb7f5f9c99246aebc3edbd853dfb147170f
Shchav <shchav@mail.ru>
2026-02-05 13:05:14 +0300

    Lesson6

--- script.js
+++ script.js
@@ -3,27 +3,37 @@
 const isNumber = function (num) {
     return !!num && isFinite(num) && !num.includes(' ');
 }
-console.log(isNumber('aaa 123 aaa'));
-console.log(isNumber(' 123 aaa '));
-console.log(isNumber(' aaa 123 '));
-console.log(isNumber('   123   '));
-console.log(isNumber('   123'));
-console.log(isNumber('123   '));
-console.log(isNumber('      '));
-console.log(isNumber(null));
-console.log(isNumber('123'));
 
-let arr = [];
-for (let i = 0; i < 7; i++) {
-    arr[i] = Math.random() * 10 + '';
-}
-for (let i = 0; i < 7; i++) {
-    if (arr[i][0] == '2' || arr[i][0] == '4')
-        console.log(arr[i])
+const guessNum = function (wishNum) {
+    let numAttemps = 10;
+
+    return function game() {
+
+        let userNum = prompt('Угадай число от 1 до 100');
+
+        function check(cond, message, isContinue) {
+            if (cond) {
+                if (isContinue == undefined) {
+                    if (confirm(message)) {
+                        numAttemps = 10;
+                        game();
+                    }
+                } else if (isContinue) {
+                    alert(message);
+                    game();
+                } else if (!isContinue) {
+                    alert(message);
+                }
+            }
+            return cond;
+        }
+        check(numAttemps == 1, 'Попытки закончились, хотите сыграть еще?') ||
+            check(userNum == null, 'Игра окончена', false) ||
+            check(!isNumber(userNum), 'Введи число!', true) ||
+            check(userNum > wishNum, 'Загаданное число меньше, осталось попыток ' + --numAttemps, true) ||
+            check(userNum < wishNum, 'Загаданное число больше, осталось попыток ' + numAttemps, true) ||
+            check(userNum == wishNum, 'Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
+    }
 }
 
-for (let i = 2; i <= 100; i++) {
-    for (var t = 2; t < i; t++)
-        if (!(i % t)) break;
-    if (t == i) console.log(i);
-}
+guessNum(23)();
