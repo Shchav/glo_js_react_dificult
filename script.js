00469df7b1fbe7a3ee823401879224b8c57bfbd4
Shchav <shchav@mail.ru>
2026-02-06 13:28:41 +0300

    Lesson7

--- script.js
+++ script.js
@@ -1,39 +1,20 @@
 'use strict'
 
-const isNumber = function (num) {
-    return !!num && isFinite(num) && !num.includes(' ');
-}
+const week = [
+    'Понедельник',
+    'Вторник',
+    'Среда',
+    'Четверг',
+    'Пятница',
+    'Суббота',
+    'Воскресенье'
+];
 
-const guessNum = function (wishNum) {
-    let numAttemps = 10;
-
-    return function game() {
-
-        let userNum = prompt('Угадай число от 1 до 100');
-
-        function check(cond, message, isContinue) {
-            if (cond) {
-                if (isContinue == undefined) {
-                    if (confirm(message)) {
-                        numAttemps = 10;
-                        game();
-                    }
-                } else if (isContinue) {
-                    alert(message);
-                    game();
-                } else if (!isContinue) {
-                    alert(message);
-                }
-            }
-            return cond;
-        }
-        check(numAttemps == 1, 'Попытки закончились, хотите сыграть еще?') ||
-            check(userNum == null, 'Игра окончена', false) ||
-            check(!isNumber(userNum), 'Введи число!', true) ||
-            check(userNum > wishNum, 'Загаданное число меньше, осталось попыток ' + --numAttemps, true) ||
-            check(userNum < wishNum, 'Загаданное число больше, осталось попыток ' + numAttemps, true) ||
-            check(userNum == wishNum, 'Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
-    }
-}
-
-guessNum(23)();
+const currentDate = new Date().toLocaleDateString('ru-RU', { weekday: 'long' });
+document.body.innerHTML =
+    week.reduce(function (htmlStr, item, index) {
+        if (item.toLowerCase() == currentDate.toLowerCase())
+            item = '<b>' + item + '</b>';
+        if (index >= 5) item = '<i>' + item + '</i>';
+        return htmlStr += item + '<br>';
+    }, '');
