717dde3f6dfae30151039516b807418267083cc6
Andrew Shchelykalnov <shchav@mail.ru>
2026-02-09 22:32:22 +0300

    Lesson9

--- script.js
+++ script.js
@@ -1,104 +1,43 @@
 'use strict'
 
-const appData = {
-    title: '',
-    screens: [],
-    screenPrice: 0,
-    adaptive: true,
-    rollback: 10,
-    allServicePrices: 0,
-    fullPrice: 0,
-    servicePercentPrice: 0,
-    services: {},
+const formatDate1 = function (now) {
+    const days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+    const months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
 
-    asking: function () {
-        appData.title = appData.prompting('Как называется ваш проект?', 'Калькулятор верстки', true);
+    let hoursStr;
+    const nowHours = now.getHours() + '';
+    if (nowHours.slice(-1) == 1 && nowHours != 11)
+        hoursStr = 'час';
+    else if ([2, 3, 4].includes(+nowHours.slice(-1)) && ![12, 13, 14].includes(+nowHours))
+        hoursStr = 'часа';
+    else hoursStr = 'часов';
 
-        for (let i = 0; i < 2; i++) {
-            let name = appData.prompting('Какие типы экранов нужно разработать?', 'Простые, сложные', true);
-            let price = 0;
-
-
-            price = +appData.prompting('Сколько будет стоить данная работа?', 10_000, false);
-
-            appData.screens.push({ id: i, name: name, price: price });
-        }
-
-        for (let i = 0; i < 2; i++) {
-            let name = appData.prompting('Какой дополнительный тип услуги нужен?', 'Тестирование', true);
-            let price = 0;
-
-            price = appData.prompting('Сколько это будет стоить?', 20_000, false);
-
-            appData.services[name + '_' + i] = +price;
-        };
-
-        appData.adaptive = confirm('Нужен ли адаптив на сайте?');
-    },
-
-    prompting: function (message, defaultInput, mustBeText) {
-        do {
-            var input = prompt(message, defaultInput);
-        } while (mustBeText == appData.isNumber(input));
-        return input;
-    },
-
-    addPrices: function () {
-        appData.screenPrice = appData.screens.reduce(function (screenPrice, screen) {
-            return screenPrice + screen.price;
-        }, 0);
-
-        for (let key in appData.services) {
-            appData.allServicePrices += appData.services[key];
-        }
-    },
-
-    getRollbackMessage: function (price) {
-        if (price >= 30000) {
-            return "Даем скидку в 10%";
-        } else if (price >= 15000 && price < 30000) {
-            return "Даем скидку в 10%";
-        } else if (price >= 0 && price < 15000) {
-            return "Скидка не предусмотрена";
-        } else {
-            return "Что-то пошло не так";
-        }
-    },
-
-    isNumber: function (num) {
-        return !isNaN(parseFloat(num)) && isFinite(num);
-    },
-
-    getFullPrice: function () {
-        appData.fullPrice = +appData.screenPrice + appData.allServicePrices;
-    },
-
-    getTitle: function () {
-        appData.title = appData.title.trim()[0].toUpperCase() + appData.title.trim().slice(1).toLowerCase();
-    },
-
-    getServicePercentPrices: function () {
-        appData.servicePercentPrice = appData.fullPrice - (appData.fullPrice * (appData.rollback / 100));
-    },
-
-    start: function () {
-        appData.asking();
-        appData.addPrices();
-        appData.getFullPrice();
-        appData.getServicePercentPrices();
-        appData.getTitle();
-
-        appData.logger();
-    },
-
-    logger: function () {
-        console.log(appData.fullPrice);
-        console.log(appData.servicePercentPrice);
-        console.log(appData.screens);
-        console.log(appData.services);
-    }
+    return 'Сегодня ' +
+        days[now.getDay()] + ', ' +
+        now.getDate() + ' ' +
+        months[now.getMonth()] + ' ' +
+        now.getFullYear() + ' года' + ', ' +
+        now.getHours() + ' ' + hoursStr + ' ' +
+        now.getMinutes() + ' минут ' +
+        now.getSeconds() + ' секунды';
 }
 
-appData.start();
+const formatDate2 = function (now) {
+    const addZero = function (num) {
+        if (num < 10) return '0' + num;
+        return num;
+    }
 
+    return addZero(now.getDate()) + '.' +
+        addZero(now.getMonth()) + '.' +
+        addZero(now.getFullYear()) + ' - ' +
+        addZero(now.getHours()) + ':' +
+        addZero(now.getMinutes()) + ':' +
+        addZero(now.getSeconds());
+}
+
+setInterval(function () {
+    document.body.innerHTML = formatDate1(new Date()) + '<br>';
+    document.body.innerHTML += formatDate2(new Date());
+}, 1000);
 
